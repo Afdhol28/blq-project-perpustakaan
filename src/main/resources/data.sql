84e97478389526997f8ab8242efe4f5246f4e461
Afdhol <afdholbasil@gmail.com>
2023-12-21 14:32:32 +0700

    push

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,16 @@
 insert into book 
 (judul,kode,penerbit,pengarang,tahun)
 values
-('Menanam Pohon','ABC123','Pustaka','Dr.Heri',2002),
-('Cara Jitu Menjadi Pengusaha','DDR321','Airlangga','Puspita',2012);
-
+('Menanam Pohon','MP123','Pustaka','Dr.Heri',2002),
+('Beternak Kambing','BK123','Pustaka','Joko',2012),
+('Menfaat Pohon','MP199','Cipta Karya','Utomo.S.pd',20021),
+('Cara Jitu Menjadi Pengusaha','CJMP321','Airlangga','Puspita',2012),
+('Reaksi Kimia','RK112','Jenius Pustaka','Dr.Kunto Wijodjo',2022),
+('Servis Motor','SM009','Gramedia','Wahyudi',2002),
+('IPA','IPA111','Mendikbud','Mendikbud',2016),
+('Bersosialisasi','B987','Gramedia','Cahyadi',2017),
+('Suku dan Budaya','SB142','Alex Media','Budi',2019),
+('Hewan Karnivora','HK332','Gramedia','Dr.Chandra',2010);
 
 insert into member 
 (nisn,nama,kelas,no_hp)
